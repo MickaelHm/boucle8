836a6be39b26b31f41a55fecf0c7076f4413fd51
MickaelHm <harmeau.mickael@gmail.com>
2018-12-20 15:38:00 +0100

    reupload bug

---
+++ boucle8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boucle8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pairs = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int counter;
+            for(counter = 0 ; counter < pairs.Length; counter++)
+            {
+                if (pairs[counter] % 2 == 0) // % (se prononce modulo) donne le reste d'une division. Dans ce cas, la division par 2 d'un nombre avec 0 comme reste. Exemple qui ne fonctionne pas, 25/2 = 12 + 1(reste) 
+                Console.WriteLine(pairs[counter]);
+            }
+        }
+    }
+}
